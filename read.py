f68841526adc737563d30903be2ead7cab1f87b5
Augustine Wang <cmw168@gmail.com>
2020-09-18 13:30:25 +0800

    calculate average reviews

--- read.py
+++ read.py
@@ -6,7 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0: # 計數每1000筆，%是求餘數
 			print(len(data)) # 才印出資料長度
-print(len(data))
-print(data[0]) # 取出data清單中的第1項
-print('----------------------------------') # 分隔號
-print(data[1]) # 取出data清單中的第2項
+print('檔案讀取完成，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
